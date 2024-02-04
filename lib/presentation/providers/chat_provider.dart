92f53d729e09ec038c3aef96c2541365a8a1030a
Alan Rodriguez <licalan.sistemas@gmail.com>
2024-02-04 11:01:14 -0600

    Initial commit

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,52 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
+
+import '../../domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+
+  final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
+
+  List<Message> messageList = [
+    Message(text: 'Hola amor!', fromWho: FromWho.me),
+    Message(text: 'Ya regresaste del trabajo!', fromWho: FromWho.me),
+  ];
+
+  Future<void> sendMessagew(String text) async {
+    if (text.isEmpty) return;
+
+    final newMessage = Message(
+      text: text,
+      fromWho: FromWho.me);
+
+      messageList.add(newMessage);
+
+      if (text.endsWith('?')) {
+        await herReply();
+      }
+
+      notifyListeners();
+      await moveScrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(herMessage);
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 30),
+      curve: Curves.easeOut
+    );
+  }
+
+}
